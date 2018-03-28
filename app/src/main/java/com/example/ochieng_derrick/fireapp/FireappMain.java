22b0fcefb6d6ff5dae974af0bdaf40853d1d60ed
Derrick Ochieng <ochiengderrick424@gmal.com>
2018-03-28 10:49:14 +0300

    Integrating firebase with android studio

---
+++ app/src/main/java/com/example/ochieng_derrick/fireapp/FireappMain.java
@@ -0,0 +1,13 @@
+package com.example.ochieng_derrick.fireapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FireappMain extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fireapp_main);
+    }
+}
